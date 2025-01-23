7290e546799a06d2ede1d7f80231ac081cc60b6f
taraliu23 <rainylty@gmail.com>
2025-01-22 23:41:00 -0500

    change: lgbtqia+ format

--- aro-and-sword/src/app/components/LGBTQIAExplanation.jsx
+++ aro-and-sword/src/app/components/LGBTQIAExplanation.jsx
@@ -1,47 +1,105 @@
+// import React, { useState } from "react";
+// // import "./LGBTQIAExplanation.css";
+
+// const LGBTQIAExplanation = () => {
+// 	const [hoveredLetter, setHoveredLetter] = useState(null);
+
+// 	const letters = [
+// 		{ id: "L", name: "Lesbian", wiki: "https://en.wikipedia.org/wiki/Lesbian", img: "/images/l.png" },
+// 		{ id: "G", name: "Gay", wiki: "https://en.wikipedia.org/wiki/Gay", img: "/images/g.png" },
+// 		{ id: "B", name: "Bisexual", wiki: "https://en.wikipedia.org/wiki/Bisexuality", img: "/images/b.png" },
+// 		{ id: "T", name: "Transgender", wiki: "https://en.wikipedia.org/wiki/Transgender", img: "/images/t.png" },
+// 		{ id: "Q", name: "Queer/Questioning", wiki: "https://en.wikipedia.org/wiki/Queer", img: "/images/q.png" },
+// 		{ id: "I", name: "Intersex", wiki: "https://en.wikipedia.org/wiki/Intersex", img: "/images/i.png" },
+// 		{ id: "A", name: "Asexual/Aromantic/Agender", wiki: "https://en.wikipedia.org/wiki/Asexuality", img: "/images/a.png" },
+// 		{ id: "+", name: "More identities", wiki: "https://en.wikipedia.org/wiki/LGBT", img: "/images/plus.png" },
+// 	];
+
+// 	return (
+// 		<div className="lgbtqia-wrapper">
+// 			<h2 className="info-title">What Does LGBTQIA+ Stand For?</h2>
+// 			<div className="letters-grid">
+// 				{letters.map((letter) => (
+// 					<div
+// 						key={letter.id}
+// 						className="letter-container"
+// 						onMouseEnter={() => setHoveredLetter(letter.id)}
+// 						onMouseLeave={() => setHoveredLetter(null)}
+// 					>
+// 						<img src={letter.img} alt={letter.id} className="letter-image" />
+// 						{hoveredLetter === letter.id && (
+// 							<div className="hover-info">
+// 								<p className="letter-name">{letter.name}</p>
+// 								<a
+// 									href={letter.wiki}
+// 									target="_blank"
+// 									rel="noopener noreferrer"
+// 									className="learn-more-button"
+// 								>
+// 									Learn More
+// 								</a>
+// 							</div>
+// 						)}
+// 					</div>
+// 				))}
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default LGBTQIAExplanation;
+
 import React, { useState } from "react";
-// import "./LGBTQIAExplanation.css";
 
 const LGBTQIAExplanation = () => {
-	const [hoveredLetter, setHoveredLetter] = useState(null);
+	const [selectedLetter, setSelectedLetter] = useState({
+		name: "Lesbian",
+		description: "A lesbian is a woman who is attracted to other women.",
+		wiki: "https://en.wikipedia.org/wiki/Lesbian",
+	});
 
 	const letters = [
-		{ id: "L", name: "Lesbian", wiki: "https://en.wikipedia.org/wiki/Lesbian", img: "/images/l.png" },
-		{ id: "G", name: "Gay", wiki: "https://en.wikipedia.org/wiki/Gay", img: "/images/g.png" },
-		{ id: "B", name: "Bisexual", wiki: "https://en.wikipedia.org/wiki/Bisexuality", img: "/images/b.png" },
-		{ id: "T", name: "Transgender", wiki: "https://en.wikipedia.org/wiki/Transgender", img: "/images/t.png" },
-		{ id: "Q", name: "Queer/Questioning", wiki: "https://en.wikipedia.org/wiki/Queer", img: "/images/q.png" },
-		{ id: "I", name: "Intersex", wiki: "https://en.wikipedia.org/wiki/Intersex", img: "/images/i.png" },
-		{ id: "A", name: "Asexual/Aromantic/Agender", wiki: "https://en.wikipedia.org/wiki/Asexuality", img: "/images/a.png" },
-		{ id: "+", name: "More identities", wiki: "https://en.wikipedia.org/wiki/LGBT", img: "/images/plus.png" },
+		{ id: "L", name: "Lesbian", description: "A lesbian is a woman who is attracted to other women.", wiki: "https://en.wikipedia.org/wiki/Lesbian", img: "/images/l.png" },
+		{ id: "G", name: "Gay", description: "Gay refers to a man who is attracted to other men.", wiki: "https://en.wikipedia.org/wiki/Gay", img: "/images/g.png" },
+		{ id: "B", name: "Bisexual", description: "Bisexual people are attracted to more than one gender.", wiki: "https://en.wikipedia.org/wiki/Bisexuality", img: "/images/b.png" },
+		{ id: "T", name: "Transgender", description: "Transgender people have a gender identity that differs from the sex they were assigned at birth.", wiki: "https://en.wikipedia.org/wiki/Transgender", img: "/images/t.png" },
+		{ id: "Q", name: "Queer", description: "Queer is an umbrella term for people who don’t identify as straight or cisgender.", wiki: "https://en.wikipedia.org/wiki/Queer", img: "/images/q.png" },
+		{ id: "I", name: "Intersex", description: "Intersex people are born with physical traits that don’t fit typical definitions of male or female bodies.", wiki: "https://en.wikipedia.org/wiki/Intersex", img: "/images/i.png" },
+		{ id: "A", name: "Asexual", description: "Asexual people experience little or no sexual attraction.", wiki: "https://en.wikipedia.org/wiki/Asexuality", img: "/images/a.png" },
+		{ id: "+", name: "Plus", description: "The '+' represents other identities not included in LGBTQIA.", wiki: "https://en.wikipedia.org/wiki/LGBT", img: "/images/plus.png" },
 	];
+
+	const handleLetterClick = (letter) => {
+		setSelectedLetter(letter);
+	};
 
 	return (
 		<div className="lgbtqia-wrapper">
 			<h2 className="info-title">What Does LGBTQIA+ Stand For?</h2>
+
 			<div className="letters-grid">
 				{letters.map((letter) => (
 					<div
 						key={letter.id}
 						className="letter-container"
-						onMouseEnter={() => setHoveredLetter(letter.id)}
-						onMouseLeave={() => setHoveredLetter(null)}
+						onClick={() => handleLetterClick(letter)}
 					>
 						<img src={letter.img} alt={letter.id} className="letter-image" />
-						{hoveredLetter === letter.id && (
-							<div className="hover-info">
-								<p className="letter-name">{letter.name}</p>
-								<a
-									href={letter.wiki}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="learn-more-button"
-								>
-									Learn More
-								</a>
-							</div>
-						)}
 					</div>
 				))}
+			</div>
+
+			<div className="letter-details-fixed">
+				<h5 className="letter-details">{selectedLetter.name}</h5>
+				<p className="letter-details">{selectedLetter.description}</p>
+				<a
+					href={selectedLetter.wiki}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="learn-more-button"
+				>
+					Learn More
+				</a>
 			</div>
 		</div>
 	);
