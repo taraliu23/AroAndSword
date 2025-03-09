7712e89416504c65f21bcbcfbef395177c22941e
taraliu23 <rainylty@gmail.com>
2025-03-09 14:54:35 -0400

    refrac: navbar

--- aro-and-sword/src/app/components/Navbar.js
+++ aro-and-sword/src/app/components/Navbar.js
@@ -5,21 +5,37 @@ import React from 'react'
 const Navbar = () => {
 	return (
 		<nav className="navbar-container">
-			{/* <div className="navbar-logo"> */}
 			<div className="navbar-logo">
 				<a href="/">
 					<img src="/images/logo.png" alt="My Logo" className="logo-image" />
 					<span className="logo-text">Aro & Sword</span>
 				</a>
 			</div>
+
 			<ul className="navbar-links">
-				<li><a href="/">[Home]</a></li>
-				{/* <li><a href="#typewriter-hero">Typewriter</a></li> */}
-				<li><a href="/bingo">[Bingo Games!]</a></li>
-				{/* <li><a href="#what-is-aromanticism">What is Aromanticism</a></li> */}
-				{/* <li><a href="#split-attraction-model">SAM</a></li> */}
-				<li><a href="/resources">[Resources]</a></li>
-				{/* <li><a href="#resources">[Get Involved]</a></li> */}
+				<li>
+					<a href="/">
+						[Home]
+					</a>
+				</li>
+
+				<li>
+					<a href="/bingo">
+						[Bingo Games!]
+					</a>
+				</li>
+
+				<li>
+					<a href="/resources">
+						[Resources]
+					</a>
+				</li>
+
+				<li>
+					<a href="/manifesto">
+						[Manifesto]
+					</a>
+				</li>
 			</ul>
 		</nav>
 	)
