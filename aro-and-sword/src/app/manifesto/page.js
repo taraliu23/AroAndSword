f777773bfd205eee0bff485924b5fcebd1c52db1
taraliu23 <rainylty@gmail.com>
2025-03-09 15:18:00 -0400

    refrac: /manifesto

--- aro-and-sword/src/app/manifesto/page.js
+++ aro-and-sword/src/app/manifesto/page.js
@@ -1,36 +1,22 @@
 
 'use client';
+
 import React from "react";
 import Navbar from '../components/Navbar'; // Import your Navbar component
-import Footer from '../components/Footer'; // Optional Footer for consistency
-import Resources from "../components/Resources";
-import { motion } from "framer-motion"
-
-
-
 import ManifestoSection from '../components/ManifestoSection'
 
 export default function ManifestoPage() {
-	return <ManifestoSection />
+	return (
+		<div className="app">
+
+			<Navbar />
+
+			<main
+				className="bingo-page">
+				<ManifestoSection />
+			</main>
+
+		</div>
+	)
 }
 
-
-// export default function ResourcesPage() {
-// 	return (
-// 		<div className="app">
-// 			<Navbar /> {/* Add a Navbar for navigation */}
-// 			<main className="bingo-page">
-// 				<h1 className="page-title">The 34 North Manifesto</h1>
-// 				<p className="page-subtitle">
-// 					Belief and Values of the Aromantic
-// 				</p>
-// 				<img
-// 					src={'/images/34N-Manifesto/1.png'}
-// 					alt={'1'}
-// 					className="img-fluid rounded"
-// 					loading="lazy" />
-
-// 			</main>
-// 		</div>
-// 	);
-// }
